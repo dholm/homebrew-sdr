1930c0b58670b1c8deb1b0d8629a4fd97855584d
David Holm <dholmster@gmail.com>
2014-12-03 21:17:30 +0100

    multimon-ng: New formula

---
+++ multimon-ng.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class MultimonNg < Formula
+  homepage "https://github.com/EliasOenal/multimon-ng"
+  head "https://github.com/EliasOenal/multimon-ng.git"
+
+  depends_on "qt" => :build
+  depends_on :x11
+
+  def install
+    mkdir "build" do
+      system "qmake",  "../multimon-ng.pro", "PREFIX=#{prefix}"
+      system "make install"
+    end
+  end
+end
