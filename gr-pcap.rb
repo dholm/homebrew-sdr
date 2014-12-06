4a766062c8ab929a6bf614da1d329bb208ce309f
David Holm <dholmster@gmail.com>
2014-12-07 00:04:26 +0100

    gr-pcap: New formula

---
+++ gr-pcap.rb
@@ -0,0 +1,23 @@
+require "formula"
+
+class GrPcap < Formula
+  homepage "https://github.com/osh/gr-pcap"
+  head "https://github.com/osh/gr-pcap.git"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gnuradio"
+  depends_on "boost"
+  depends_on "cppunit"
+
+  def install
+    mkdir "build" do
+      args = %W[
+        -DPYTHON_LIBRARY='#{%x(python-config --prefix).chomp}/lib/libpython2.7.dylib'
+      ] + std_cmake_args
+
+      system "cmake", "..", *args
+      system "make install"
+    end
+  end
+end
