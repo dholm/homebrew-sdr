8258d667c442e33a44c87e8f5676181e56a0e089
David Holm <dholmster@gmail.com>
2014-12-07 22:26:26 +0100

    gr-sdr: New formula

---
+++ gr-sdr.rb
@@ -0,0 +1,24 @@
+require "formula"
+
+class GrSdr < Formula
+  homepage "https://github.com/pothosware/gr-sdr"
+  head "https://github.com/pothosware/gr-sdr.git"
+
+  depends_on "cmake" => :build
+  depends_on "swig" => :build
+  depends_on "gnuradio"
+  depends_on "boost"
+  depends_on "cppunit"
+  depends_on "soapysdr"
+
+  def install
+    mkdir "build" do
+      args = %W[
+        -DPYTHON_LIBRARY='#{%x(python-config --prefix).chomp}/lib/libpython2.7.dylib'
+      ] + std_cmake_args
+
+      system "cmake", "..", *args
+      system "make install"
+    end
+  end
+end
