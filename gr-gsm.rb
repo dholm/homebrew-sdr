7d3d4d921f9bdb8114aebcc155750e3bc0c38698
David Holm <dholmster@gmail.com>
2015-08-23 20:26:08 +0200

    gr-gsm: Patch merged upstream

--- gr-gsm.rb
+++ gr-gsm.rb
@@ -4,8 +4,6 @@ class GrGsm < Formula
   homepage "https://github.com/ptrkrysik/gr-gsm"
   head "https://github.com/ptrkrysik/gr-gsm.git"
 
-  patch :DATA
-
   depends_on "cmake" => :build
   depends_on "swig" => :build
   depends_on "boost"
@@ -23,27 +21,3 @@ class GrGsm < Formula
     end
   end
 end
-
-__END__
---- i/lib/misc_utils/extract_immediate_assignment_impl.cc
-+++ w/lib/misc_utils/extract_immediate_assignment_impl.cc
-@@ -28,7 +28,7 @@
- #include <grgsm/gsmtap.h>
- #include <unistd.h>
- #include <map>
--#include <endian.h>
-+#include <grgsm/endian.h>
- #include <boost/foreach.hpp>
- 
- #include "extract_immediate_assignment_impl.h"
---- i/lib/misc_utils/extract_system_info_impl.cc
-+++ w/lib/misc_utils/extract_system_info_impl.cc
-@@ -31,7 +31,7 @@
- #include <iterator>
- #include <algorithm>
- #include <iostream>
--#include <endian.h>
-+#include <grgsm/endian.h>
- #include <boost/foreach.hpp>
- extern "C" {
-     #include <osmocom/gsm/gsm48_ie.h>
