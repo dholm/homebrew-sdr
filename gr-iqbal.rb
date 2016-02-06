6444da4cb73a3d97f8688ef4465b56abf42248a4
David Holm <dholmster@gmail.com>
2016-02-06 19:37:23 +0100

    gr-iqbal: 0.37.2

--- gr-iqbal.rb
+++ gr-iqbal.rb
@@ -1,11 +1,14 @@
 class GrIqbal < Formula
   homepage "http://www.osmocom.org/"
+  url "http://git.osmocom.org/gr-iqbal/snapshot/gr-iqbal-0.37.2.tar.xz"
+  sha256 "933d047e5be51020bc9a15386ddea7870103fb080c249af8aedf2dd4f1b499b2"
   head "git://git.osmocom.org/gr-iqbal"
 
   depends_on "cmake" => :build
-  depends_on "gnuradio"
+
   depends_on "boost"
   depends_on "fftw"
+  depends_on "gnuradio"
 
   patch :DATA
 
