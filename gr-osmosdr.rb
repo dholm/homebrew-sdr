5e134d2503faa2d689411c4d7b8b4cbd5bfbfba4
David Holm <dholmster@gmail.com>
2016-02-06 19:41:50 +0100

    gr-osmosdr: 0.1.4

--- gr-osmosdr.rb
+++ gr-osmosdr.rb
@@ -1,8 +1,11 @@
 class GrOsmosdr < Formula
   homepage "http://sdr.osmocom.org/trac/wiki/GrOsmoSDR"
+  url "http://git.osmocom.org/gr-osmosdr/snapshot/gr-osmosdr-0.1.4.tar.xz"
+  sha256 "1945d0d98fd4b600cb082970267ec2041528f13150422419cbd7febe2b622721"
   head "git://git.osmocom.org/gr-osmosdr"
 
   depends_on "cmake" => :build
+
   depends_on "gnuradio"
   depends_on "gr-fosphor"
   depends_on "gr-iqbal"
