266e595d3fab0949e7cc57924cc7517d8b03212a
David Holm <dholmster@gmail.com>
2016-02-06 17:30:36 +0100

    cubicsdr: 0.1.24-alpha

--- cubicsdr.rb
+++ cubicsdr.rb
@@ -2,12 +2,16 @@ require "formula"
 
 class Cubicsdr < Formula
   homepage "https://github.com/cjcliffe/CubicSDR"
+  url "https://github.com/cjcliffe/CubicSDR/archive/v0.1.24-alpha.tar.gz"
+  sha256 "5bbcea5f9b6e8b91ac3b5d532ca247ce38285ae434c3c64f62bd032f6c7b994c"
   head "https://github.com/cjcliffe/CubicSDR.git"
 
   depends_on "cmake" => :build
-  depends_on "liquid-dsp"
+
   depends_on "fftw"
   depends_on "librtlsdr"
+  depends_on "liquid-dsp"
+  depends_on "soapysdr"
   depends_on "wxwidgets"
 
   def install
