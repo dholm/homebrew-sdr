e22ffea8d25c3975f0205f5d27971b0073a0a3c9
David Holm <dholmster@gmail.com>
2016-02-06 19:43:00 +0100

    libosmo-dsp: 0.3

--- libosmo-dsp.rb
+++ libosmo-dsp.rb
@@ -2,12 +2,15 @@ require "formula"
 
 class LibosmoDsp < Formula
   homepage "http://osmocom.org"
+  url "http://git.osmocom.org/libosmo-dsp/snapshot/libosmo-dsp-0.3.tar.xz"
+  sha256 "bad3b544ff5b65c4db486f951258cdaefc0fe2a6b60d497416aec7574931eae5"
   head "git://git.osmocom.org/libosmo-dsp"
 
-  depends_on "libtool" => :build
-  depends_on "automake" => :build
   depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
+
   depends_on "fftw"
 
   def install
