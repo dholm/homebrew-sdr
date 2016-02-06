15eb2cd740400b760c480e8629519b9ce12962c3
David Holm <dholmster@gmail.com>
2016-02-06 19:49:00 +0100

    osmo-sdr: 0.1

--- osmo-sdr.rb
+++ osmo-sdr.rb
@@ -1,8 +1,11 @@
-class Libosmosdr < Formula
+class OsmoSdr < Formula
   homepage "http://osmocom.org"
+  url "http://cgit.osmocom.org/osmo-sdr/snapshot/osmo-sdr-0.1.tar.xz"
+  sha256 "ac488768ad735dd6ca0ba594643f831e2b62747244da9a233264c11a96bc1f96"
   head "git://git.osmocom.org/osmo-sdr.git"
 
   depends_on "cmake" => :build
+
   depends_on "libusb"
 
   def install
