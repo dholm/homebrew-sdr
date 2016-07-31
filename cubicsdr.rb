9f65d75d783a1eaf7d8fe3548ff6281d6ab4770a
David Holm <dholmster@gmail.com>
2016-07-31 18:06:05 +0200

    cubicsdr: 0.2.0b5

--- cubicsdr.rb
+++ cubicsdr.rb
@@ -2,8 +2,9 @@ require "formula"
 
 class Cubicsdr < Formula
   homepage "https://github.com/cjcliffe/CubicSDR"
-  url "https://github.com/cjcliffe/CubicSDR/archive/v0.1.24-alpha.tar.gz"
-  sha256 "5bbcea5f9b6e8b91ac3b5d532ca247ce38285ae434c3c64f62bd032f6c7b994c"
+  url "https://github.com/cjcliffe/CubicSDR/archive/0.2.0-beta-rc5.tar.gz"
+  version "0.2.0b5"
+  sha256 "20f9cfeb24717a177b1d57692c54985d16d576fcbed97e9a9415a8bfa9a5510d"
   head "https://github.com/cjcliffe/CubicSDR.git"
 
   depends_on "cmake" => :build
