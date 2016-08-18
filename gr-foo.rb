9f95ba3689c4d4d3598e57939c60cfeb608b4299
rxseger <rseger@gmx.co.uk>
2016-08-17 20:25:51 -0700

    Add gr-foo

---
+++ gr-foo.rb
@@ -0,0 +1,24 @@
+class GrFoo < Formula
+  homepage "https://github.com/bastibl/gr-foo"
+  head "https://github.com/bastibl/gr-foo.git"
+
+  depends_on "cmake" => :build
+  depends_on "swig" => :build
+  depends_on "gnuradio"
+  depends_on "boost"
+  depends_on "cppunit"
+
+  def install
+    mkdir "build" do
+      ENV.append "LDFLAGS", "-Wl,-undefined,dynamic_lookup"
+      # Point Python library to existing path or CMake test will fail.
+      args = %W[
+        -DCMAKE_SHARED_LINKER_FLAGS='-Wl,-undefined,dynamic_lookup'
+        -DPYTHON_LIBRARY='#{HOMEBREW_PREFIX}/lib/libgnuradio-runtime.dylib'
+      ] + std_cmake_args
+
+      system "cmake", "..", *args
+      system "make", "install"
+    end
+  end
+end
