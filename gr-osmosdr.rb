1230db240194cb7be1b83b6b00ea126a0117772a
David Holm <dholmster@gmail.com>
2018-02-13 21:16:23 +0100

    gr-osmosdr: Fix python dependency
    Fixes #26, #28

--- gr-osmosdr.rb
+++ gr-osmosdr.rb
@@ -4,9 +4,16 @@ class GrOsmosdr < Formula
   sha256 "1945d0d98fd4b600cb082970267ec2041528f13150422419cbd7febe2b622721"
   head "git://git.osmocom.org/gr-osmosdr"
 
-  depends_on "cmake" => :build
+  resource "Markdown" do
+    url "https://files.pythonhosted.org/packages/1d/25/3f6d2cb31ec42ca5bd3bfbea99b63892b735d76e26f20dd2dcc34ffe4f0d/Markdown-2.6.8.tar.gz"
+    sha256 "0ac8a81e658167da95d063a9279c9c1b2699f37c7c4153256a458b3a43860e33"
+  end
 
-  depends_on "Cheetah" => :python
+  resource "Cheetah" do
+    url "https://files.pythonhosted.org/packages/cd/b0/c2d700252fc251e91c08639ff41a8a5203b627f4e0a2ae18a6b662ab32ea/Cheetah-2.4.4.tar.gz"
+    sha256 "be308229f0c1e5e5af4f27d7ee06d90bb19e6af3059794e5fd536a6f29a9b550"
+  end
+
   depends_on "gnuradio"
   depends_on "gr-fosphor"
   depends_on "gr-iqbal"
@@ -14,6 +21,14 @@ class GrOsmosdr < Formula
   depends_on "pothosware/pothos/soapysdr"
 
   def install
+    ENV["CHEETAH_INSTALL_WITHOUT_SETUPTOOLS"] = "1"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    resources.each do |r|
+      r.stage do
+        system "python", *Language::Python.setup_install_args(libexec)
+      end
+    end
+
     mkdir "build" do
       ENV.append "LDFLAGS", "-Wl,-undefined,dynamic_lookup"
       # Point Python library to existing path or CMake test will fail.
