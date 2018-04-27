0793ef40e8ad8c17956f276df986c4fa77a3402c
David Holm <dholmster@gmail.com>
2018-04-27 21:39:17 +0200

    libosmocore: 0.9.6

--- libosmocore.rb
+++ libosmocore.rb
@@ -2,11 +2,9 @@ require "formula"
 
 class Libosmocore < Formula
   homepage "http://bb.osmocom.org/trac/wiki/libosmocore"
-  url "http://git.osmocom.org/libosmocore/snapshot/libosmocore-0.9.0.tar.gz"
-  sha256 "d09489630396941bdb4d123aba1a17595fd3ac9684a7a4874bc14387854bc2b4"
+  url "http://git.osmocom.org/libosmocore/snapshot/libosmocore-0.9.6.tar.gz"
+  sha256 "7accf049891991c6a7b2b98e439cc5c7effe48ce623227745d0c24b4bc3411fc"
   head "git://git.osmocom.org/libosmocore"
-
-  patch :DATA
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -14,6 +12,8 @@ class Libosmocore < Formula
   depends_on "pkg-config" => :build
 
   depends_on "pcsc-lite"
+  depends_on "python@2"
+  depends_on "talloc"
 
   def install
     system "autoreconf", "-i"
@@ -22,18 +22,3 @@ class Libosmocore < Formula
     system "make", "install"
   end
 end
-
-__END__
-diff --git i/src/stats.c w/src/stats.c
-index 73b2703..c722ee3 100644
---- i/src/stats.c
-+++ w/src/stats.c
-@@ -345,7 +345,7 @@ int osmo_stats_reporter_send(struct osmo_stats_reporter *srep, const char *data,
- {
- 	int rc;
- 
--	rc = sendto(srep->fd, data, data_len, MSG_NOSIGNAL | MSG_DONTWAIT,
-+	rc = sendto(srep->fd, data, data_len, MSG_DONTWAIT,
- 		&srep->dest_addr, srep->dest_addr_len);
- 
- 	if (rc == -1)
