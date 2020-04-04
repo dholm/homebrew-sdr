da0b3245324a3550a23bfd0ab984cfa65f369d2a
Bryan Irvine <sparctacus@gmail.com>
2020-04-04 12:11:03 -0500

    Update liquid-dsp.rb

--- liquid-dsp.rb
+++ liquid-dsp.rb
@@ -4,7 +4,7 @@ class LiquidDsp < Formula
   homepage "http://liquidsdr.org/"
   head "https://github.com/jgaeddert/liquid-dsp.git"
   # inspectrum needs a recent version of liquid-dsp and the latest release is from 2012.
-  url "https://github.com/jgaeddert/liquid-dsp.git", :revision => "1191179b786703b3af20abf7e1404d91099b335d"
+  #url "https://github.com/jgaeddert/liquid-dsp.git", :revision => "1191179b786703b3af20abf7e1404d91099b335d"
 
   depends_on "fftw"
   depends_on "autoconf" => :build
